6d937ee3088725a84c3b1eee66a9e247725a54d9
Tor Lillqvist <tml@collabora.com>
2018-05-09 13:21:19 +0300

    Put executables in the root of the zip file, and add demo vbs and VB6 exe, and demo docs

--- createZip.sh
+++ createZip.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 tstamp=$(TZ=UTC0 date +'%Y%m%d.%H%M').$(git log -1 --pretty=format:%h)
+zip=coleat-$tstamp.zip
 
-zip coleat-$tstamp.zip bin/*.exe bin/*.dll
+zip $zip README.txt
+
+(cd bin; zip ../$zip *.exe *.dll)
+
+zip $zip demo.vbs demo.exe demo.frm u1.odt u2.doc
