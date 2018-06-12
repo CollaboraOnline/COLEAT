e021f722fdbc9c60bef8efebca705dc242a62f97
Tor Lillqvist <tml@collabora.com>
2018-06-12 14:07:25 +0300

    Create also a source tarball

--- createZip.sh
+++ createZip.sh
@@ -5,10 +5,14 @@ COLEAT_VERSION_MINOR=$(grep COLEAT_VERSION_MINOR include/coleat-version.h | awk 
 COLEAT_GIT_HEAD=$(grep COLEAT_GIT_HEAD include/coleat-git-version.h  | awk '{print substr($3,1,7)}')
 
 tstamp=$(TZ=UTC0 date +'%Y%m%d.%H%M')
-zip=coleat-$COLEAT_VERSION_MAJOR.$COLEAT_VERSION_MINOR-$tstamp.$COLEAT_GIT_HEAD.zip
+prefix=coleat-$COLEAT_VERSION_MAJOR.$COLEAT_VERSION_MINOR-$tstamp.$COLEAT_GIT_HEAD
+zip=$prefix.zip
+tarball=$prefix.tar.gz
 
 zip $zip README.txt
 
 (cd bin; zip ../$zip *.exe *.dll)
 
 zip $zip demo.vbs demo.exe demo.frm u1.odt u2.docx
+
+git archive --prefix=$prefix/ -o $tarball $COLEAT_GIT_HEAD
